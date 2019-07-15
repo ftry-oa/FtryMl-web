025c040c38ab349ad1f2e5f1d4489be22e6b16a6
jianfehuang(黄剑枫) <jianfehuang@tencent.com>
2019-07-15 21:43:21 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 
-// var $root = document.getElementsByClassName('root')[0]
-// $root.innerHTML = 'Hello FtryMl-web'
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  },
+});
